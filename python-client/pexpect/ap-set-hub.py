91c8eca83d7abd6d18064b252468e9eded80970e
dogkeeper886 <jack.w.tseng@outlook.com>
2020-07-01 08:54:13 +0800

    add pexpect folder

---
+++ python-client/pexpect/ap-set-hub.py
@@ -0,0 +1,33 @@
+import pexpect
+import sys
+
+# argument check
+if len(sys.argv) != 2:
+    print('[AP_IP_ADDRESS]')
+    exit(1)
+
+# input apIp
+apIp = sys.argv[1]
+print('apIp:', apIp)
+
+# start ap conect
+con = 'ssh ' + apIp
+child = pexpect.spawn(con, encoding='utf-8', logfile=sys.stdout)
+
+# login ap
+child.expect('Please login:')
+child.sendline('super')
+child.expect('password :')
+child.sendline('sp-admin')
+child.expect('rkscli:')
+
+# set hub
+child.sendline('set hub host aprqa.ruckuswireless.com')
+child.expect('OK')
+
+# log out
+child.sendline('exit')
+
+# end ap connect
+if child.isalive():
+    child.close()
